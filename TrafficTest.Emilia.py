b17b71cbb8b7965d4c7b970a85b1710bb4ef77c2
CBR2021Pi <thomas.r.janssen@outlook.com>
2021-12-27 19:34:25 +0100

    Erste Eweiterungen für 4 Ampeln.

--- TrafficTest.Emilia.py
+++ TrafficTest.Emilia.py
@@ -1,10 +1,27 @@
 import PiTraffic as Ampel
 import time as Zeit
 
+# Festlegung der Nordampel
+NordampelROT = Ampel.Traffic("NORTH", "RED")
+NordampelGELB = Ampel.Traffic("NORTH", "YELLOW")
+NordampelGRÜN = Ampel.Traffic("NORTH", "GREEN")
+
+# Festlegung der Westtampel
+#WestampelROT = Ampel.Traffic("WEST", "RED")
+#WestampelGELB = Ampel.Traffic("WEST", "YELLOW")
+#WestampelGRÜN = Ampel.Traffic("WEST", "GREEN")
+
+
 # Festlegung der Ostampel
 OstampelROT = Ampel.Traffic("EAST", "RED")
 OstampelGELB = Ampel.Traffic("EAST", "YELLOW")
 OstampelGRÜN = Ampel.Traffic("EAST", "GREEN")
+
+# Festlegung der Westtampel
+WestampelROT = Ampel.Traffic("WEST", "RED")
+WestampelGELB = Ampel.Traffic("WEST", "YELLOW")
+WestampelGRÜN = Ampel.Traffic("WEST", "GREEN")
+
 
 # Festlegung der Südampel
 Pizzaampelrot = Ampel.Traffic("SOUTH", "RED")
@@ -17,33 +34,67 @@ try:
     # Endlos-Schleife "while True:" wiederholt die 
     # folgenden Befehle immer wieder von oben nach unten
     while True:
-        Pizzahupe.off()
+        Pizzahupe.off ()
         
-        # Halten
+        # +++ Phase 1 +++
+        
+        # Nord und Süd dürfen fahren
+        NordampelROT.off()
+        NordampelGELB.off()
+        NordampelGRÜN.on()
+        
+        #SüdampelROT.off()
+        #SüdampelGELB.off()
+        #SüdampelGRÜN.on()
+        
+        
+        # Ost und West müssen anhalten
         OstampelROT.on()
         OstampelGELB.off()
         OstampelGRÜN.off() 
         
+        WestampelROT.on()
+        WestampelGELB.off()
+        WestampelGRÜN.off()
+        
         Zeit.sleep(3)
+        
+        # +++ Phase 2 +++
         
         # Bereitmachen
         OstampelROT.on()
         OstampelGELB.on()
         OstampelGRÜN.off()
         
+        WestampelROT.on()
+        WestampelGELB.on()
+        WestampelGRÜN.off()
+        
         Zeit.sleep(2)
+        
+        # +++ Phase 3 +++
         
         # Fahren
         OstampelROT.off()
         OstampelGELB.off()
         OstampelGRÜN.on()
         
+        WestampelROT.off()
+        WestampelGELB.off()
+        WestampelGRÜN.on()
+        
         Zeit.sleep(3)
+        
+        # +++ Phase 4 +++
         
         # Bremsen
         OstampelROT.off()
         OstampelGELB.on()
         OstampelGRÜN.off()
+        
+        WestampelROT.off()
+        WestampelGELB.on()
+        WestampelGRÜN.off()
         
         Zeit.sleep(2)
                
